6aa622c2fb699b301803ca132b7e4a94066b55b8
SofiaLemos22 <sofialemos091@gmail.com>
2024-10-21 20:31:34 -0300

    Se eliminaron archivos

--- ejercicio.py
+++
@@ -1,5 +0,0 @@
-try:
-    # Código que puede generar una excepción
-    result = 10 / 0  # Esto genera una excepción ZeroDivisionError
-except ZeroDivisionError as e:  # Captura la excepción y la asigna a la variable 'e'
-    print(f"Ocurrió un error: {e}")  # 'e' contiene el mensaje de la excepción
